0647b98baa82a8533bb43233c9ee770b9f79afe8
Faisal Niloy <fmniloy2016@gmail.com>
2022-05-29 08:29:37 +0100

    Added questions

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+  }
+
+  var questions = [
+    'What is your favourite color?',
+    'What is your favourite novel?'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
+        // body
         body: Column(children: [
-          Text('First question?'),
-          RaisedButton(child: Text('Answer 1'), onPressed: null),
-          RaisedButton(child: Text('Answer 1'), onPressed: null),
-          RaisedButton(child: Text('Answer 1'), onPressed: null)
+          Text(questions[questionIndex]),
+          RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => print('Answer chosen 1')),
+          RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer chosen 2')),
+          RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => {print('Answer chosen 3')})
         ]),
       ),
     );
